8175d9d4af4aee8c7d4b9a6702cc163cd8f3832c
JamesCCHsu <james_cc_hsu@Hotmail.com>
2015-07-15 19:02:36 -0700

    main.js.v0.3

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+var firstdollar = true;
+var firsticecream = true;
+
 var paper = 0; paperFixed = paper.toFixed(2);
 var icecream = 0;
 var drool = 0; droolFixed = drool.toFixed(2);
 var employee = 0;
 
+
+
 function paperClick(number){
     paper = (paper + number)*(1+0.0001*drool);
 	paperFixed = paper.toFixed(2);
     document.getElementById("paperFixed").innerHTML = paperFixed;
+		if (firstdollar == true){
+			document.getElementById("prompt").innerHTML = "Congratulations on your first Walrus Dollar! Why not spend it on some icecream?";
+			document.getElementById("prompt2").innerHTML = "You can purchase 1 icecream with W$10";
+			firstdollar = false;
+		}
+
 };
 
 function icecreamClick(number){
@@ -14,10 +25,13 @@ function icecreamClick(number){
 		icecream = icecream + number;
 		paper = paper - 10;
 		paperFixed = paper.toFixed(2);
+		document.getElementById("icecream").innerHTML = icecream;
 		document.getElementById("paperFixed").innerHTML = paperFixed;
-	} else {}
-    document.getElementById("icecream").innerHTML = icecream;
-	
+			if (firsticecream == true){
+				document.getElementById("prompt").innerHTML = "Congratulations on your first icecream! Icecream is used to make sure Walrus and her employees do not run out of drool. Their drool will run out without icecream!";
+				firsticecream = false;
+			}
+	}
 };
 
 function employeeClick(number){
@@ -47,7 +61,7 @@ window.setInterval(function() {
 
 var droolDecay = 0.05
 window.setInterval(function() {
-	if (drool > 0.1){
+	if (drool > 0.01){
 		drool = drool - droolDecay;
 		drool = drool + (0.005*icecream);
 		droolFixed = drool.toFixed(2);
@@ -58,13 +72,12 @@ window.setInterval(function() {
 var droolEmployee = 0.1
 window.setInterval(function() {
 		if (employee >= 1){
-		paper = paper + 1;
-		paperFixed = paper.toFixed(2);
-		document.getElementById("paperFixed").innerHTML = paperFixed;
-		drool = drool - droolEmployee;
-		document.getElementById("droolFixed").innerHTML = droolFixed;
+			if (drool > 0)
+				paper = paper + 1;
+				paperFixed = paper.toFixed(2);
+				document.getElementById("paperFixed").innerHTML = paperFixed;
+				drool = drool - droolEmployee;
+				document.getElementById("droolFixed").innerHTML = droolFixed;
 	}
 }, 1000);
-
-
 
